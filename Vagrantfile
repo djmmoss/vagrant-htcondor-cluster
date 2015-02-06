b6f88f732a3a38232d12b906af48eb1d3da9d318
Duncan Moss <djm.moss@gmail.com>
2015-02-06 11:01:39 +1100

    setting up condor in docker

--- Vagrantfile
+++ Vagrantfile
@@ -45,7 +45,6 @@ Vagrant.configure("2") do |config|
                 v.name = "Backtest Slave " + num.to_s
             end
         
-
             # Provision Slaves with Puppet
             slave.vm.provision "puppet" do |puppet|
                 puppet.manifests_path = "puppet/manifests"
--- puppet/manifests/slave.pp
+++ puppet/manifests/slave.pp
@@ -1,20 +1,29 @@
-class {"r":}
-
-#r::installp{"xts":}
-#r::installp{"Rcpp":}
 include stdlib
+include epel
 
 stage { "pre":
   before => Stage["main"],
   }
+
+package { "device-mapper-libs":
+    ensure => latest,
+}
 
 service { "iptables": 
     ensure => "stopped",
 }
 
 class { "docker" :
-}
+    tcp_bind    => 'tcp://127.0.0.1:4243',
+    socket_bind => 'unix:///var/run/docker.sock',
+    }
 
+docker::image{ "jimwhite/condor-centos6" :}
+
+docker::run { "condor1" :
+    image => "jimwhite/condor-centos6",
+    hostname => "slave1.1"
+}
 
 #class {"htcondor" :
     #managers => ["10.2.0.2"],
